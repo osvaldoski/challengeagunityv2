9113234815f66f4ebe06f8d1581273a72ee9421d
Osvaldo Chacon <osvaldo143143@gmail.com>
2024-01-11 04:36:49 +1000

    Initial commit

---
+++ population.js
@@ -0,0 +1,32 @@
+const population = {
+    "countries": [
+        {
+            "name": "Australia",
+            "menPopulation": 12.84,
+            "womenPopulation": 12.94,
+            "youthPopulation": 5.71
+
+        },
+        {
+            "name": "Ghana",
+            "menPopulation": 16.46,
+            "womenPopulation": 16.38,
+            "youthPopulation": 14.62
+
+        },
+        {
+            "name": "Indonesia",
+            "menPopulation": 139.13,
+            "womenPopulation": 137.23,
+            "youthPopulation": 82.06
+
+        },
+        {
+            "name": "Kenya",
+            "menPopulation": 27.32,
+            "womenPopulation": 27.66,
+            "youthPopulation": 24.42
+
+        }
+    ]
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+HTML project designed to showcase the integration of JavaScript and CSS to read and display JSON data.
+
+Project Overview
+Files
+index.html: The main HTML file where the JSON data is displayed.
+styles.css: CSS file for styling the index.html.
+
+How It Works
+index.html: The main entry point of the project. Open this file in your browser to see the JSON data displayed on the webpage.
+
+styles.css: Stylesheet for enhancing the visual appeal of the index.html.
+
+Usage
+Clone the repository:
+git clone https://github.com/osvaldoski/challengeagunityv2.git
+
+
+Open index.html in your preferred web browser.
